75f0eec17d46c6fdf1b3eed297501bc0e84db119
Legend9T9 <150684451+Legend9T9@users.noreply.github.com>
2024-10-04 18:09:32 +0500

    filed created b ali haider

---
+++ app/doctors/page.tsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+type Props = {}
+
+export default class page extends PureComponent<Props> {
+  render() {
+    return (
+      <div>DocTors Page Added By Ali Haider!</div>
+    )
+  }
+}
